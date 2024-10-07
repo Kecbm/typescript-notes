c0c77cc8cb9231ea21f6aa64bdf2b5f35e846bea
Klecianny Melo <kleciannymelo@gmail.com>
2024-10-07 14:58:34 -0300

    :tada: first example of code in typescript

---
+++ chapter 1/1. Basic syntax.ts
@@ -0,0 +1,7 @@
+// Type declarations
+
+var nameOfUser: string = "Klecianny";
+var age: number = 28;
+var isStudent: boolean = true;
+
+console.log(`Olá, meu nome é ${nameOfUser}, eu tenho ${age} anos e ${isStudent ? null : "não"} sou estudante.`);
